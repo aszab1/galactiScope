2a92fd8842c0b5179a9e7f32cfb061830379a110
NyashaDZT <nyashapatrickdzvoti@live.co.uk>
2023-11-23 13:04:25 +0000

    wednesday afternoon @aszab1

--- src/components/RandomPictures.jsx
+++ src/components/RandomPictures.jsx
@@ -10,28 +10,73 @@ import Col from 'react-bootstrap/Col';
 
 export default function RandomPictures(){
 
+  const imagesPerPage = 20
   const pictures = useLoaderData()
-  const [ apodImages, setapodImages ] = useState([])
+  const [ apodImages, setApodImages ] = useState([])
+  const [ currentPage, setCurrentPage] = useState(1)
+  const [ loading, setLoading] = useState(false)
+
+
+ //const totalPages = Math.ceil(apodImages.length / imagesPerPage)
 
   useEffect (() => {
-    
-
-    console.log(pictures)
+    fetchMoreImages()
   }, [])
+
+  const fetchMoreImages = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=pW4NzGSYWgso3Cx1tqSsFRffDSMVUxF8y9yuaezz&count=${imagesPerPage}`)
+        const newImages = await response.json()
+        setApodImages(newImages)
+    }catch(error) {
+      console.error('Error fetching random images:', error)
+    } finally {
+      setLoading(false)
+    }
+
+  }
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+    console.log(currentPage)
+    console.log(apodImages)
+  } 
+  
+  const handleNext = () => {
+   if (currentPage < Math.ceil(pictures.length / imagesPerPage)) {
+      console.log('buttonClicked') 
+    } else {
+
+    fetchMoreImages()
+    setCurrentPage(currentPage + 1)
+    console.log(imagesPerPage)
+    console.log(currentPage)
+    console.log(apodImages)
+    }
+  }
+
+  const indexOfLastImage = currentPage * imagesPerPage;
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+  const currentImages = apodImages.slice(indexOfFirstImage, indexOfLastImage);
 
   return (
     <>
+    <button onClick={handlePrevious} disabled={currentPage === 1}>Previous page</button>
+    <button onClick={handleNext}>Next page</button>
     <h1 className='text-center bold display-3 mb-4'>Anything</h1>
     <Container fluid>
       <Row className='picture-row' style={{  marginTop: '5px' }}>
-        { pictures.map(picture => {
+        { currentImages.map(picture => {
           const { date, explanation, url, title } = picture
           return (
             <Col 
               as={Link}
               key={title} 
-              xs={6} 
-              md={4} 
+              xs={3} 
+              md={3} 
               lg={3}
               style={ { backgroundImage: `url(${url})` } }
               to={`/RandomPictureS/${date}`}
